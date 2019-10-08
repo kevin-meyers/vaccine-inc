119c5bf7f60c7b2d1bb390a3cf4749b1f6aa9027
Kevin <kevinm1776@gmail.com>
2019-10-07 18:25:47 -0700

    Preperation for logging.

--- person.py
+++ person.py
@@ -1,6 +1,9 @@
+from random import random
+
+
 class Person:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, _id):
+        self.id = _id
         self.vaccinated = False
         self.infected = False
         self.interact_list = []
@@ -9,4 +12,26 @@ class Person:
 
 
     def update(self, virus):
-        pass  # kevin pls
+        if self.vaccinated:
+            # log not because of vaccine
+            return self.id, 'vaccinated'
+
+        if self.infected:
+            # log not because already infected
+            return self.id, 'infected'
+
+    
+    def end_step(self):
+        self.interact_list = []
+
+    def is_infected(self):
+        which_infected = [
+            interactor.id for interactor in self.interact_list 
+            if random() <= virus.infect_rate
+        ]
+                # log infected by interaction with which_infected
+
+
+        self.infected = True
+
+
