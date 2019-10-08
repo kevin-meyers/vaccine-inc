a258fbb5448b89ca464a50e56b9f09af84b59093
= <=>
2019-10-07 17:54:43 -0700

    Add person.py

---
+++ person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, id):
+        self.id = id
+        self.vaccinated = False
+        self.infected = False
+        self.interact_list = []
+
+    # if not vaccinated and not infected, list of infectors
+
+
+    def update(self, virus):
+        pass  # kevin pls
